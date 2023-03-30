158ff8f352040a63942f983ef85e423414bde751
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-03-30 18:15:27 -0500

    Made progress on the projects section

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="projects-section">
+    <p class="projects-intro">{{ projectsIntro }}</p>
+
+    <div class="download-cv-button">
+      <a :href="cv">
+        <p>Download <span class="cv">CV</span></p>
+      </a>
+    </div>
+
+    <h3>Projects</h3>
+  </div>
 </template>
 
 <script>
@@ -8,7 +18,10 @@ import portfolioInfo from '../portfolio-info.json';
 
 export default {
   data() {
-    return {};
+    return {
+      projectsIntro: portfolioInfo.projectsIntro,
+      cv: portfolioInfo.cv,
+    };
   },
 };
 </script>
